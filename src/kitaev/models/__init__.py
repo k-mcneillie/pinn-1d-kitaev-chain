9a9c08b7d0817bc556249a7faf23c8a48cd0397f
Konner McNeillie <konner.mcneillie@icloud.com>
2026-08-28 07:14:29 +0100

    chore: update

--- src/kitaev/models/__init__.py
+++ src/kitaev/models/__init__.py
@@ -1,10 +1,11 @@
 from .siren_chiral import ChiralToBdGAdapter, SirenPINNChiral
 from .siren_dual import SirenPINNDualHead
-from .siren_single import SirenPINN
+from .siren_single import RayleighEnergyAdapter, SirenPINN
 
 __all__ = [
     "SirenPINNDualHead",
     "SirenPINN",
     "SirenPINNChiral",
     "ChiralToBdGAdapter",
+    "RayleighEnergyAdapter",
 ]
